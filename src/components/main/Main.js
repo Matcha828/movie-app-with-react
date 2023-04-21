eaa9385813984e2b180ce7426b096af1d1ef66d6
Wei Chen <43740276+Matcha828@users.noreply.github.com>
2023-04-20 22:01:54 -0400

    added the slideshow component

---
+++ src/components/main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import MainContent from '../content/main-content/MainContent';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <MainContent />
+      Main
+    </div>
+  );
+};
+
+export default Main;
